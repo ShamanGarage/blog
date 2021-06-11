68a7405e40e7d5f82bbab683c93bb4e92fba00fe
pabgre <pablosan@tutanota.com>
2021-06-11 12:37:30 +0200

    Styles update

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import '../fonts/css/atkinson.css'
 import './App.css';
 import React from 'react';
 import { Route, Switch, Link } from "react-router-dom";
@@ -10,15 +11,15 @@ function App() {
           <div>
             <nav>
 
+                <a href="https://shamansgarage.com/#/blog-info">
+                <div className ="nav-btn close">
+                <i class="fa fa-times" aria-hidden="true"></i>
+                </div>
+                </a>
+
                 <Link to="/">
                 <div className ="nav-btn">
-                <p>Home</p>
-                </div>
-                </Link>
-
-                <Link to="/about">
-                <div className ="nav-btn">
-                <p>About</p>
+                <i class="fa fa-i-cursor" aria-hidden="true"></i>
                 </div>
                 </Link>
             </nav>
@@ -28,9 +29,6 @@ function App() {
             <Route exact path="/">
               <h1>Home</h1><br/>
               <Link to="/2021/T1">¿Esto funciona?</Link>
-            </Route>
-            <Route path="/about">
-              <h1>About</h1>
             </Route>
             <Route exact path="/:date/:post/">
               <BlogEntry/>
